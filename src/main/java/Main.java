0ff734b0baba5e30aaa6359bf1f79961edeff867
Qutlet <maciej.bigos@onet.com.pl>
2021-05-20 18:59:29 +0200

    initial

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args){
         DataBase dataBase = DataBase.getSelf();
         dataBase.connect();
-        //System.out.println("test");
+        System.out.println("test");
     }
 
 }
