5cb152d7885e8704b79fe6996f49ca6a7874a5ab
Qutlet <maciej.bigos@onet.com.pl>
2021-05-20 18:58:32 +0200

    initial

---
+++ src/main/java/DataBase.java
@@ -0,0 +1,38 @@
+import java.sql.*;
+
+public class DataBase {
+
+    private static DataBase self = new DataBase();
+    public static DataBase getSelf() {return self;}
+    Connection connection = null;
+
+    public Connection getConnection() {return connection;}
+
+    public void connect() {
+        try {
+            String dbURL = "jdbc:sqlserver://localhost:1433;databaseName=Basketball;integratedSecurity=true;";
+            String user = "sa";
+            String pass = "secret";
+            connection = DriverManager.getConnection(dbURL);
+            if (connection != null) {
+                DatabaseMetaData dm = connection.getMetaData();
+                System.out.println("Driver name: " + dm.getDriverName());
+                System.out.println("Driver version: " + dm.getDriverVersion());
+                System.out.println("Product name: " + dm.getDatabaseProductName());
+                System.out.println("Product version: " + dm.getDatabaseProductVersion());
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                if (connection != null && !connection.isClosed()) {
+                    connection.close();
+                }
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+
+}
